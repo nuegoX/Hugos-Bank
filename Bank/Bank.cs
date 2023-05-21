2e3475eaaa356975184aae14aff5e10c97bdc5dd
Hugo <nuego@proton.me>
2023-05-21 12:44:21 +0200

    :)

--- Bank/Bank.cs
+++ Bank/Bank.cs
@@ -1,18 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bank
 {
-    internal class Bank
+    public class Bank
     {
+        public Bank()
+        {
+
+        }
+        public List<Account> accounts = new List<Account>();
+
+        private String RandomizeCardnumber ()
+        {
+            Random random = new Random();
+            String bin = "123456"; // Replace with your desired BIN
+            String cardNumber = bin;
+
+            // Generate the remaining 10 digits randomly
+            for (int i = 0; i < 10; i++)
+            {
+                int randomNumber = random.Next(0, 10);
+                cardNumber += randomNumber;
+            }
+
+            return cardNumber;
+        }
+        public void CreateAccount (string Username, string Password, int Pincode, int Cardnumber, float Balance)
+        {
+            Account currentUser = new Account(Username, Password, Pincode, Cardnumber, Balance);
+            accounts.Add(new Account(Username, Password, Pincode, Cardnumber, Balance));
+        }
+        public void RegisterAccount (string Username, string Password, int Pincode, int Cardnumber, float Balance)
+        {
+            Account currentUser = new Account(Username, Password, Pincode, Cardnumber, Balance);
+            accounts.Add(currentUser);
+        }
+        public void LoginUser ()
+        {
+
+        }
 
     }
 
-    public abstract class Account
+    public class Account
     {
+        public String Username;
+        public String Password;
+        public int Pincode;
+        public int Cardnumber;
+        public float Balance;
+
+        public Account(string Username, string Password, int Pincode, int Cardnumber, float Balance)
+        {
+            this.Username = Username;
+            this.Password = Password;
+            this.Cardnumber = Cardnumber;
+            this.Balance = Balance;
+            this.Pincode = Pincode;
+        }
 
     }
 }
--- Bank/Form1.cs
+++ Bank/Form1.cs
@@ -7,31 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace Bank
 {
     public partial class Form1 : Form
     {
         public static Form1 Instance;
+        Bank bank = new Bank();
         public Form1()
         {
             InitializeComponent();
             Instance = this;
+            
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load (object sender, EventArgs e)
         {
             // TODO:
             // Create a bank object and then through inheritance all possible bank accounts through loading in a text file that is created if it doesn't exist already.
-
+           
 
             
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            String Username = textBox1.Text;
-            String Password = textBox2.Text;
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            foreach (Account acc in bank.accounts)
+            {
+                // Perform your logic here with each account
+                // For example, you can check if the username and password match an account
+                if (acc.Username == username && acc.Password == password)
+                {
+                    // Account found, do something
+                    // ...
+                    break; // Exit the loop since we found a matching account
+                }
+            }
         }
+
     }
 }
